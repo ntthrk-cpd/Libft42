2243eaf197d04a28faf2351f3918527c9afa8206
ntthrk@h <ntthrk.cpn1994@gmail.com>
2023-02-16 06:59:12 +0700

    add ft_meemcpy.c but it's it is not success

---
+++ ft_memcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheepan <ncheepan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 06:29:05 by ncheepan          #+#    #+#             */
+/*   Updated: 2023/02/16 06:57:22 by ncheepan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n){
+	while (n-- > 0)
+		dest_char[n] = ;
+	
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+   const char src[50] = "http://www.tutorialspoint.com";
+   char dest[50];
+   strcpy(dest,"Heloooo!!");
+   printf("Before memcpy dest = %s\n", dest);
+   memcpy(dest, src, strlen(src)+1);
+   printf("After memcpy dest = %s\n", dest);
+   
+   return(0);
+}
