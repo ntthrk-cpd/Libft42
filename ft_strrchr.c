7c7f4a5c5ca7e91937572ed88af84baaa2737b9b
ntthrk@h <ntthrk.cpn1994@gmail.com>
2023-02-16 20:42:24 +0700

    exit retrun on ft_strchr.c add ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,28 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ncheepan <ncheepan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/16 09:36:41 by ncheepan          #+#    #+#             */
-/*   Updated: 2023/02/16 20:27:46 by ncheepan         ###   ########.fr       */
+/*   Created: 2023/02/16 14:32:14 by ncheepan          #+#    #+#             */
+/*   Updated: 2023/02/16 20:40:21 by ncheepan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *str, int c)
+char	*ft_strrchr(const char *str, int c)
 {
 	while (c > 127)
 		c -= 128;
 	while (*str)
-	{
+		*str++;
+	*str--;
+        while (*str){
 		if (*str == c)
 			return ((char *)str);
-		*str++;
+		*str--;
 	}
 	if (c == '\0')
 		return ((char *)str);
-	return (NULL);
+        return (NULL);
 }
