58c7587f5d15d9e75fc0f815fa2adcab001c6f96
ntthrk@h <ntthrk.cpn1994@gmail.com>
2023-02-17 11:25:27 +0700

    modified ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,28 +6,20 @@
 /*   By: ncheepan <ncheepan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 06:29:05 by ncheepan          #+#    #+#             */
-/*   Updated: 2023/02/16 06:57:22 by ncheepan         ###   ########.fr       */
+/*   Updated: 2023/02/17 11:23:01 by ncheepan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n){
-	while (n-- > 0)
-		dest_char[n] = ;
-	
-}
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char	*char_dest;
+	char	*char_src;
 
-#include <stdio.h>
-#include <string.h>
-
-int main () {
-   const char src[50] = "http://www.tutorialspoint.com";
-   char dest[50];
-   strcpy(dest,"Heloooo!!");
-   printf("Before memcpy dest = %s\n", dest);
-   memcpy(dest, src, strlen(src)+1);
-   printf("After memcpy dest = %s\n", dest);
-   
-   return(0);
+	char_dest = (char *)dest;
+	char_src = (char *)src;
+	while (n--)
+		*char_dest++ = *char_src++;
+	return (dest);
 }
