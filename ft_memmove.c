09825c2c11ab92e79ecbdb6a232067256df60ed5
ntthrk@h <ntthrk.cpn1994@gmail.com>
2023-02-17 13:41:19 +0700

    add ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheepan <ncheepan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 13:02:44 by ncheepan          #+#    #+#             */
+/*   Updated: 2023/02/17 13:39:39 by ncheepan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char	*char_dest;
+	char	*char_src;
+
+	char_dest = (char *)dest;
+	char_src = (char *)src;
+	if (n == 0)
+		return (dest);
+	n--;
+	while (n--)
+		*char_dest++ = *char_src++;
+	return ((void *)dest);
+}
