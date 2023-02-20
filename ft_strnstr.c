0238fb204f4989ada28a7ce3415067a4e5a80f07
Nattarika Cheepandung <ncheepan@z2t7c2.42bangkok.com>
2023-02-20 13:56:53 +0700

    add function for ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,24 +6,35 @@
 /*   By: ncheepan <ncheepan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 22:49:00 by ncheepan          #+#    #+#             */
-/*   Updated: 2023/02/16 23:28:28 by ncheepan         ###   ########.fr       */
+/*   Updated: 2023/02/20 13:56:10 by ncheepan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
 
-int	main()
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	printf("%c\n", ft_strnstr("lorem ipsum dolor sit amet", "lorem", 15));
-	printf("%c\n", ft_strnstr("lorem ipsum dolor sit amet", "ipsum", 15));
-	printf("%c\n", ft_strnstr("lorem ipsum dolor sit lorem ipsum dolor", "ipsum", 35));
-	printf("%c\n", ft_strnstr("lorem ipsum dolor sit amet", "", 10));
-	printf("%c\n", ft_strnstr("lorem ipsum dolor sit amet", "ipsumm", 30));
-	printf("%c\n", ft_strnstr("lorem ipsum dolor sit amet", "dol", 30));
-	printf("%c\n", ft_strnstr("lorem ipsum dolor sit amet", "consectetur", 30));
-	printf("%c\n", ft_strnstr("lorem ipsum dolor sit amet", "sit", 10));
-	printf("%c\n", ft_strnstr("lorem ipsum dolor sit amet", "dolor", 15));
-	printf("%c\n", ft_strnstr("lorem ipsum dolor sit amet", "dolor", 0));
-	return (0);
+	int	count_i;
+	int	count_j;
+
+	count_i = 0;
+	count_j = 0;
+	if (*needle)
+	{
+		while (count_i < len && haystack[count_i] != '\0')
+		{
+			while (haystack[count_i + count_j] == needle[count_j]
+				&& needle[count_j] != '\0'
+				&& (count_i + count_j) < len)
+			{
+				if (needle[count_j + 1] == '\0')
+					return ((char *)haystack + count_i);
+				count_j++;
+			}
+			count_j = 0;
+			count_i++;
+		}
+		return (NULL);
+	}
+	return ((char *)haystack);
 }
