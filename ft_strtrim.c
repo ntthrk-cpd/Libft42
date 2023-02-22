c3b3922f40a4e82c6a095106c2ba51ee6bf4efd9
ntthrk@h <ntthrk.cpn1994@gmail.com>
2023-02-22 23:33:09 +0700

    edit norminete for ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,24 +6,24 @@
 /*   By: ncheepan <ncheepan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 10:21:24 by ncheepan          #+#    #+#             */
-/*   Updated: 2023/02/22 22:31:59 by ncheepan         ###   ########.fr       */
+/*   Updated: 2023/02/22 23:32:18 by ncheepan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*Set_Back(char *s)
+char	*set_back(char *s)
 {
-        int     count;
+	int	count;
 
-        count = ft_strlen(s);
+	count = ft_strlen(s);
 	while (s[count] == '\t' || s[count] == '\n' || s[count] == ' ')
-                s[count--] = '\0';
-	return (s);		
+		s[count--] = '\0';
+	return (s);
 }
-char	*Set_Front(char *s)
-{
 
+char	*set_front(char *s)
+{
 	int	count;
 
 	count = 0;
@@ -31,56 +31,45 @@ char	*Set_Front(char *s)
 		count++;
 	return (s + count);
 }
-char	*Remove_Char(char *s, char *set)
+
+char	*remove_char(char *s, char *set)
 {
-	int	count;
-	int	pos;
+	int		count;
+	int		pos;
 	char	*str_new;
 
 	count = 0;
 	pos = 0;
-	str_new = s;
-	printf("rm is %s\n", set);
-	if (set[0] != '\0')
+	str_new = ft_strdup(s);
+	if (*set)
 	{
 		while (s[pos] != '\0')
 		{
-			
 			if (s[pos] != set[0])
-			{
-				str_new[count] = s[pos];		
-				printf("%c\t" , str_new[count]);
-				count++;
-			}
-			pos++;  	
+				str_new[count++] = s[pos];
+			pos++;
 		}
-		str_new[count] = '\0';
-		printf("-----> %s\n", s);
-		Remove_Char((char *)s, (char *)set + 1);
+		if (count > 0)
+			str_new[count] = '\0';
+		if (*++set)
+			remove_char((char *)str_new, (char *)set);
 	}
-	return (s);
+	return (str_new);
 }
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str_trim;
-	size_t		len_s1;
+	size_t	len_s1;
 
 	len_s1 = ft_strlen(s1);
 	str_trim = (unsigned char *)malloc(len_s1);
-
-	if(!*str_trim)
+	if (!*str_trim)
 	{
-		str_trim = Set_Front((char *)s1);
-		str_trim = Set_Back(str_trim);
-		str_trim = Remove_Char(str_trim, (char *)set);
-		return (str_trim);			
+		str_trim = set_front((char *)s1);
+		str_trim = set_back(str_trim);
+		str_trim = remove_char(str_trim, (char *)set);
+		return (str_trim);
 	}
 	return (NULL);
-}
-
-int	main()
-{
-	printf("sum = %s\n", ft_strtrim("\t   Hello Wellcome   \n", "eo"));
-	return (0);
 }
