7939b06f1d7b73425080b0c058f9aacd1257d7ef
ntthrk@h <ntthrk.cpn1994@gmail.com>
2023-02-25 13:28:58 +0700

    add ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncheepan <ncheepan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/25 13:13:43 by ncheepan          #+#    #+#             */
+/*   Updated: 2023/02/25 13:28:30 by ncheepan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		index;
+	int		len;
+	char	*str;
+
+	index = 0;
+	len = ft_strlen(s) + 1;
+	str = (char *)malloc(sizeof((char *) * len);
+	if (!s || !f || !str)
+		return (NULL);
+	while (index < len)
+		str[index] = f(i, s[i++]);
+	return (str);
+}
