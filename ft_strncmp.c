1b985c35420722f26a8febe2a3c542c428986bde
ntthrk@h <ntthrk.cpn1994@gmail.com>
2023-03-23 17:02:05 +0700

    update norminett

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ncheepan <ncheepan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 21:20:27 by ncheepan          #+#    #+#             */
-/*   Updated: 2023/03/18 11:33:46 by ncheepan         ###   ########.fr       */
+/*   Updated: 2023/03/23 17:01:14 by ncheepan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
--- ft_substr.c
+++ ft_substr.c
@@ -5,13 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ncheepan <ncheepan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD
-/*   Created: 2023/03/23 16:17:47 by ncheepan          #+#    #+#             */
-/*   Updated: 2023/03/23 16:43:27 by ncheepan         ###   ########.fr       */
-=======
 /*   Created: 2023/02/20 17:05:43 by ncheepan          #+#    #+#             */
-/*   Updated: 2023/03/18 17:17:49 by ncheepan         ###   ########.fr       */
->>>>>>> cd3a0751c9cf5d00391b0ae7b9a4e9bff74de27d
+/*   Updated: 2023/03/23 16:59:12 by ncheepan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +18,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	unsigned int	count;
 
 	count = 0;
-<<<<<<< HEAD
 	sub_str = (char *)malloc(sizeof(char *) * len);
 	if (sub_str)
 	{
@@ -32,27 +26,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 			sub_str[count++] = s[start++];
 		sub_str[count] = '\0';
 		return (sub_str);
-=======
-	sub_str = (char *)malloc(sizeof(char *) * len + 1);
-	if (sub_str && 
-		ft_strlen(s) >= start)
-	{
-		while (count < len
-			&& s[count + start] != '\0')
-		{
-			sub_str[count] = s[count + start];
-			count++;
-		}
-		sub_str[count + start] = '\0';
-		return ((char *)sub_str);
->>>>>>> cd3a0751c9cf5d00391b0ae7b9a4e9bff74de27d
 	}
 	return (NULL);
 }
 /*
 int	main()
 {
-<<<<<<< HEAD
 	char *s = ft_substr("lorem ipsum dolor sit amet", 0, 10);
 	printf("\n%s\n", s);
 	free(s);
@@ -77,8 +56,5 @@ int	main()
 
 
 	return (0);
-=======
-	printf("%s\n",ft_substr("hello World", 1, 1));
 
->>>>>>> cd3a0751c9cf5d00391b0ae7b9a4e9bff74de27d
 }*/
