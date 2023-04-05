1d98c44ef9302276613609667d3f6e9c1935b3af
Nattarika Cheepandung <ncheepan@z2t16c2.42bangkok.com>
2023-04-05 18:21:53 +0700

    update striteri error back

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,87 +6,70 @@
 /*   By: ncheepan <ncheepan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 10:21:24 by ncheepan          #+#    #+#             */
-/*   Updated: 2023/03/30 17:54:32 by ncheepan         ###   ########.fr       */
+/*   Updated: 2023/04/05 18:20:24 by ncheepan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_getsize(char const *s, char const *set)
+size_t	getback(char const *s1, char const *set)
 {
-	int	count;
-	int	pos1;
-	int	pos2;
+	size_t	back;
 
-	count = ft_strlen(s);
-	pos1 = 0;
-	while (set[pos1] != '\0')
-	{
-		pos2 = 0;
-		while (s[pos2] != '\0')
-		{
-			if (s[pos2] == set[pos1])
-				count--;
-			pos2++;
-		}
-		pos1++;
-	}
-	return (count);
+        back = ft_strlen(s1) - 1;
+        while (ft_strchr(set, s1[back]) && back >= 0 )
+                back--;
+	printf("\nback : %ld\n" , back);
+	return (back + 1);
 }
 
-char	*remove_char(char *s, char *set)
+size_t	getfront(char const *s1, char const *set)
 {
-	int		count;
-	int		pos;
-	char	*str_new;
+	size_t	front;
 
-	count = 0;
-	pos = 0;
-	str_new = ft_strdup(s);
-	if (set)
-	{
-		while (s[pos] != '\0')
-		{
-			if (s[pos] != set[0])
-				str_new[count++] = s[pos];
-			pos++;
-		}
-		str_new[count] = '\0';
-		if (set[1] != '\0')
-			return (remove_char(str_new, set + 1));
-	}
-	return (str_new);
+      	front = 0;
+        while (ft_strchr(set, s1[front]) && s1[front] != '\0')
+                front++;
+	printf("\nfront : %ld\n", front);
+	return (front);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*str_trim;
-	size_t	size_str;
-
-	size_str = ft_getsize(s1, set);
-	str_trim = (char *)malloc(sizeof(char) * (size_str + 1));
-	if (str_trim)
-	{
-		str_trim = remove_char((char *)s1, (char *)set);
-		return (str_trim);
-	}
-	return (NULL);
+	char	*str_new;
+	size_t		size;
+	
+	if (!set)
+		return ((char *)s1);
+	if (!s1)
+		return (NULL);
+	printf("\ns1:%s | set:%s\t", s1, set);
+	size = getback(s1, set) - getfront(s1, set);
+	printf("\n%ld\n", size);
+	if (size == 0)
+		return (ft_strdup(""));
+	str_new = (char *)malloc(sizeof(char) * (size  + 1));
+	if (!str_new)
+		return (NULL);
+	str_new = ft_substr(s1, getfront(s1, set), size);	
+	printf("\n%s\n", str_new);
+	return (str_new);
 }
 /*
 int	main()
 {
-	char *s = ft_strtrim("\t\t   \nHello World\t \t", "Ho");
+	char *s = ft_strtrim("xxxHello Worlddxxdd", "Hxd");
 	printf("\n%s\n", s);
 
 	char s1[] = "lorem \n ipsum \t dolor \n sit \t amet";
-	char *strtrim1 = ft_strtrim(s1, " ");
+	char *strtrim1 = ft_strtrim(s1, "loremt");
 	printf("1\n%s\n----------------------\n%s\n\n", s1, strtrim1);
 
 	char s2[] = "lorem ipsum dolor sit amet";
 	char *strtrim2 = ft_strtrim(s2, "te");
 	printf("2\n%s\n-----------------------\n%s\n\n", s2, strtrim2);
 	
-	char s3[] = " lorem ipsum dolor sit amet";
+	char s3[] = "   lorem ipsum dolor sit amet";
 	char *strtrim3 = ft_strtrim(s3, "l ");
 	printf("3\n%s\n------------------------\n%s\n\n", s3, strtrim3);
 
@@ -98,4 +81,5 @@ int	main()
 	char *strtrim5 = ft_strtrim(s5, " ");
 	printf("5\n%s\n-------------------------\n%s\n\n", s5, strtrim5);
 	return (0);
-}*/
+}
+*/
