8088f1d17c4c5a72f0cf8d50b508625f1205d416
ntthrk@h <ntthrk.cpn1994@gmail.com>
2023-04-08 10:23:08 +0700

    modified ft_split but function is not test

--- ft_split.c
+++ ft_split.c
@@ -6,17 +6,64 @@
 /*   By: ncheepan <ncheepan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 20:37:45 by ncheepan          #+#    #+#             */
-/*   Updated: 2023/04/06 19:00:12 by ncheepan         ###   ########.fr       */
+/*   Updated: 2023/04/08 10:21:48 by ncheepan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_countword(char *s, char c)
+void	ft_memfree(char **str_list)
 {
-	int	count;
-	int	index;
-	int	boolean;
+	size_t	i;
+
+	i = 0;
+	while (str_list[i])
+		free(str_list[i++]);
+	free(str_list);
+	return (NULL);
+}
+
+size_t	ft_countchar(char *s, char c)
+{
+	size_t	count;
+
+	count++;
+	while (*s++ != c && s)
+		count++;
+	return(count);
+}
+
+char **ft_getlist(char **str_list, char *s, char c, size_t pos)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (s[i] == c)
+	{
+		while(s[i] == c)
+			i++;
+	}
+	else if (s[i] != c)
+	{	
+		if(!(str_list[pos] = (char *)malloc(sizeof(char) * ft_countchar(s, c) + 1)
+			return (NULL);
+		while (s[i] != c && s[i])
+			str_list[pos][j++] = s[i++];
+		str_list[pos][j] == '\0';	
+	}
+	if (s[i]);
+		return (ft_getlist(str_list, s + i, ++pos));	
+	str_list[++pos] = 0;
+	return (str_list);
+}
+
+size_t	ft_countword(char *s, char c)
+{
+	size_t	count;
+	size_t	index;
+	size_t	boolean;
 
 	count = 0;
 	index = 0;
@@ -47,10 +94,11 @@ char	**ft_split(char const *s, char c)
 	pos = 0;
 	count_word = ft_countword((char *)s, c);
 	printf("\ncount : %ld\n", count_word);
-	str_list = (char **)malloc(sizeof(char *) * (count_word + 1));
+	if(!(str_list = (char **)malloc(sizeof(char *) * (count_word + 1))))
+		return (ft_memfree(str_list));
 	if (str_list)
 	{
-			str_list[pos] = ft_getlist();
+		str_list = ft_getlist(str_list, s, c, pos);
 	}
 	return (str_list);
 }
